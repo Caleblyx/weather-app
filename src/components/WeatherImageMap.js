d6225917d1b7b6c5ac003fa69f5c82c5c213b3c4
Caleb <caleblyx@outlook.com>
2022-12-26 13:56:58 +0800

    fix urls in WeatherImageMap.js

--- src/components/WeatherImageMap.js
+++ src/components/WeatherImageMap.js
@@ -6,12 +6,12 @@ import clouds from '../images/cloudy.png';
 import drizzle from '../images/drizzle.png';
 
 const WeatherImageMapBG = {
-  "Rain" : "url(/images/rain-bg.png)",
-  "Thunderstorm" : "url(/images/thunderstorm-bg.png)",
-  "Snow" : "url(/images/snow-bg.png)",
-  "Clear" : "url(/images/clear-bg.png)",
-  "Clouds" : "url(/images/clouds-bg.png)",
-  "Drizzle" : "url(/images/drizzle-bg.png)"
+  "Rain" : "url(images/rain-bg.png)",
+  "Thunderstorm" : "url(images/thunderstorm-bg.png)",
+  "Snow" : "url(images/snow-bg.png)",
+  "Clear" : "url(images/clear-bg.png)",
+  "Clouds" : "url(images/clouds-bg.png)",
+  "Drizzle" : "url(images/drizzle-bg.png)"
 };
 
 const WeatherImageMap = {
